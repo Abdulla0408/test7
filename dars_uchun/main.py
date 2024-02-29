d50df77783a08e700132edf0909e6d4764c533e8
Abdulla <abdullahayitboyev0408@gmail.com>
2024-03-01 03:57:33 +0500

    eighth commit

---
+++ dars_uchun/main.py
@@ -0,0 +1,32 @@
+# import csv
+
+# with open("info_csv.csv", mode = "w") as file:
+#     writer = csv.write(file, delimiter = ",", lineterminator = "\n")
+#     writer.writerow(
+#         ["NAME", "LAST NAME", "AGE", "ADDRES"]
+#     )
+
+
+# users = [
+#     {'username':'tonny','followers':'987','online':True},
+#     {'username':'tonny','followers':'1000','online':False},
+#     {'username':'tommyjimmy','followers':'1285','online':True},
+#     {'username':'micky','followers':'822','online':True},
+# ]
+
+# for user in users:
+#      print(user)
+
+#      with open("info_csv.csv", mode = "w") as file:
+#           writer = csv.writer(file, delimiter = ",", lineterminator = "\n")
+#           writer.writerow(
+#                user)
+          
+
+
+
+print(__name__)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+# import csv
+
+# with open("test_dict.csv", mode = "r") as file:
+#     content = csv.reader(file)
+#     for user in content:
+#         print( )
+    
+# ----------------------------------------------------------------
+
+import csv 
+user = 
